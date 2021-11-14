ee540ffc21397424c3e6ba68467ea10fbb976c20
John Ketterer <johnmketterer@gmail.com>
2021-11-13 23:07:07 -0500

    post deploy

--- imServiceProvider/settings.py
+++ imServiceProvider/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = (env('DEBUG') == 'True')
 
 ALLOWED_HOSTS = [
     # '*',
