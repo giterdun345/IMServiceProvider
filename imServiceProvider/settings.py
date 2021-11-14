0b92eabaa81d2d1507b72ada2629045f2617e567
John Ketterer <johnmketterer@gmail.com>
2021-11-13 20:07:54 -0500

    dhango-heroku added

--- imServiceProvider/settings.py
+++ imServiceProvider/settings.py
@@ -12,11 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env()
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
