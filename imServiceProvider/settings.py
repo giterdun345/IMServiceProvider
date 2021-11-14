28ce3e5a031f7d51cb9a4140422e4279ee5408ec
John Ketterer <johnmketterer@gmail.com>
2021-11-13 21:24:13 -0500

    settings update

--- imServiceProvider/settings.py
+++ imServiceProvider/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['https://boiling-meadow-93698.herokuapp.com/']
+ALLOWED_HOSTS = ['boiling-meadow-93698.herokuapp.com/']
 
 
 # Application definition
