a2702527af17bac2da3630d3b0a0b8c2ef846781
John Ketterer <johnmketterer@gmail.com>
2021-11-15 00:54:09 -0500

    updated FolderCustomFieldChanged

--- api/views.py
+++ api/views.py
@@ -1,24 +1,16 @@
 # import re
 # from django.shortcuts import render
-from os import link
-from re import sub
-from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import PrioritySubmission
-from .serializers import PrioritySubmissionSerializer
-from datetime import datetime, timedelta
-from django.utils import timezone
-import time
-import requests
 import environ
+from .uitilityFunctions import getFolderDataThenSave
 
 env = environ.Env()
 environ.Env.read_env()
 
 
-@api_view(['POST'])
+@ api_view(['POST'])
 def wrike_incoming(request):
     # enter into array from wrike webhook
     incoming = request.data[0]  # ["data"][0] for Folder GET
@@ -48,80 +40,17 @@ def wrike_incoming(request):
         "Authorization": "Bearer " + auth_token
     }
 
+    print(eventType)
     # # all eventTypes are sent from Wrike, continues the fuction if the eventType is correct ICONSISTENT WRIKE API
-    if eventType in ['FolderCreated', 'FolderUpdated', 'ProjectStatusChanged', 'CustomFieldUpdated']:
-        try:
-            response = requests.get(getFolderUrl, headers=headers)
-            print(f"Stage 1:  {response.status_code}")
-            getWrikeData = response.json()
-            print(f"Stage 2: {getWrikeData}")
+    if eventType in ['FolderCreated', 'FolderUpdated', 'ProjectStatusChanged', 'CustomFieldUpdated', 'FolderCustomFieldChanged']:
+        getFolderDataThenSave(getFolderUrl, headers)
 
-            if response.status_code == 200:
-                getWrikeData = getWrikeData.get('data')[0]
-                print(f"Stage 3: {getWrikeData}")
-            else:
-                print(f"Stage 3: Failed")
-                return Response("No Folder Found in Wrike", status=status.HTTP_200_OK)
+        def populateGSheets():
+            return ""
 
-            def findCustomDataField(id):
-                """Searches custom field by ID to return its value"""
-                return next((item["value"] for item in getWrikeData["customFields"] if item["id"] == id), None)
-
-            statement = findCustomDataField("IEABAVGPJUACJTNA")
-            workImpact = findCustomDataField("IEABAVGPJUACJTN7")
-            solutionRequirements = findCustomDataField('IEABAVGPJUACJTOA')
-            possibleSolutions = findCustomDataField('IEABAVGPJUACJTOB')
-            linksProvided = findCustomDataField("IEABAVGPJUACJTO4")
-            submitter = findCustomDataField('IEABAVGPJUACJTOC')
-            solutionDeveloper = findCustomDataField("IEABAVGPJUACJ7JQ")
-
-            if submitter == "":
-                submitter = 'Anonymous'
-
-            extractedWrikeData = {
-                "folderId": getWrikeData["id"],
-                "folderPermalink": getWrikeData["permalink"],
-                "title": getWrikeData["title"],
-                "startDate": getWrikeData["createdDate"],
-                "updatedDate": getWrikeData["updatedDate"],
-                "linksProvided": linksProvided,
-                "workImpact": workImpact,
-                "statement": statement,
-                "submitter": submitter,
-                "possibleSolutions": possibleSolutions,
-                "solutionRequirements": solutionRequirements,
-                "solutionDeveloper": solutionDeveloper,
-                "inputContributor": "r",
-                "agreer": "r",
-                "decider": "r",
-                "implementor": "r",
-                "acceptor": "r",
-            }
-
-            serializedFromWrike = PrioritySubmissionSerializer(
-                data=extractedWrikeData)
-
-            def deletePriorInstance():
-                """Deletes all prior instances in db based on FolderId"""
-                instancesOld = PrioritySubmission.objects.filter(
-                    folderId=extractedWrikeData["folderId"]).order_by("-updatedDate")
-                for instance in instancesOld[1:]:
-                    print(f"Searching ... {instance}")
-                    instance.delete()
-                return "Older Instances deleted, new one added"
-
-            if serializedFromWrike.is_valid():
-                print('Made it to is valid')
-                serializedFromWrike.save()
-                result = deletePriorInstance()
-                return Response(result, status=status.HTTP_200_OK)
-            else:
-                return Response(serializedFromWrike.errors, status=status.HTTP_200_OK)
-        except:
-            import sys
-            print(str(sys.exc_info()))
-        # return Response('Event Type is not what we are looking for...', status=status.HTTP_200_OK)
-    return Response('Event Type is not what we are looking for...', status=status.HTTP_200_OK)
+        return Response("Older Instances deleted, new one added", status=status.HTTP_200_OK)
+    else:
+        return Response('Event Type is not what we are looking for...', status=status.HTTP_200_OK)
 
 
 @ api_view(["GET"])
